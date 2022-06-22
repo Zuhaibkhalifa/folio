ad5e7ecc0a562e02d1a1a0b3b9709c5321416cc9
Zuhaib khalifa <zuhaibkhalifa@gmail.com>
2022-06-22 11:50:53 +0530

    ecperince

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./services.css";
 
 function services() {
-  return <section id="services">sdf</section>;
+  return (
+    <section id="services">
+      <h5>What i offer</h5>''
+      <h2>Services</h2>;
+      <div className="container services__container">
+        <article className="services">
+          <div className="services__head"></div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default services;
